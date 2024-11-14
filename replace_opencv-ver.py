707ec299c485e126970eb9dba3130394b01fca65
katsunori-waragai <huyuhiko1128@gmail.com>
2024-11-14 19:24:06 +0900

    use Path

--- replace_opencv-ver.py
+++ replace_opencv-ver.py
@@ -1,12 +1,5 @@
 from pathlib import Path
 
-patterns = {
-    "opencv-python": "4.0.0.21",
-    "opencv-contrib-python": "4.0.0.21",
-    "numpy": "1.22.1",
-}
-
-pyproject="pyproject.toml"
 
 def replace_version(line, patterns):
     for pat, version in patterns.items():
@@ -17,7 +10,14 @@ def replace_version(line, patterns):
     return line
 
 
+patterns = {
+    "opencv-python": "4.0.0.21",
+    "opencv-contrib-python": "4.0.0.21",
+    "numpy": "1.22.1",
+}
+
+pyproject = Path("pyproject.toml")
 olines = [replace_version(line, patterns) for line in open(pyproject)]
 
-outfile = "pyproject_new.toml"
+outfile = Path("pyproject_new.toml")
 open(outfile, "wt").writelines(olines)
