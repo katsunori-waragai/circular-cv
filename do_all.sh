8b0c340e0ed740a3e9b0ed24df8906b01ae8874a
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-11-14 15:49:51 +0900

    && is needed

--- do_all.sh
+++ do_all.sh
@@ -2,6 +2,5 @@
 for v in $(cat candidate.txt)
 do
   echo "#" $v;
-  echo python3 -m pip install opencv-python==$v;
-  echo python3 circular.py $v;
+  echo "python3 -m pip install opencv-python==$v && python3 circular.py $v" ;
 done
