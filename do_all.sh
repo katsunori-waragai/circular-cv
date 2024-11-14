7c2bd79245bdadecd8181e8f3eb876f202c456b0
katsunori-waragai <huyuhiko1128@gmail.com>
2024-11-14 16:38:53 +0900

    script working in PC Ubuntu

--- do_all.sh
+++ do_all.sh
@@ -4,7 +4,7 @@ echo "python3 -m pip uninstall opencv-contrib-python opencv-contrib-python-headl
 for v in $(cat candidate.txt)
 do
   echo "#" $v;
-  echo "python3 -m pip uninstall opencv-python opencv-python-headless"
-  echo "rm -r venv/lib/python3.8/site-packages/__pycache__/"
+  echo "python3 -m pip uninstall --yes opencv-python opencv-python-headless"
+  echo "rm -rf venv/lib/python3.8/site-packages/__pycache__/"
   echo "python3 -m pip install opencv-python==$v && python3 circular.py $v" ;
 done
