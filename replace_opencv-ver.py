325b9209017f4c152078ee99f997a9bc58019105
katsunori-waragai <huyuhiko1128@gmail.com>
2024-11-14 19:13:42 +0900

    use dict

--- replace_opencv-ver.py
+++ replace_opencv-ver.py
@@ -3,15 +3,21 @@ patterns = [
     "opencv-contrib-python",
 ]
 
+patterns = {
+    "opencv-python": "4.0.0.21",
+    "opencv-contrib-python": "4.0.0.21",
+    "numpy": "1.22.1",
+}
+
 pyproject="pyproject.toml"
 
-version = "4.0.0.21"
-
-
 def replace_version(line, patterns):
-    pos = max([line.find(pat) for pat in patterns])
-    new_string = line[:pos] + f'opencv-python=={version}",\n' if pos > -1 else line
-    return new_string
+    for pat, version in patterns.items():
+        pos = line.find(pat)
+        if pos > -1:
+            line = line[:pos] + f'{pat}=={version}",\n'
+            break
+    return line
 
 
 olines = []
