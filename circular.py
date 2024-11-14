6e135a520bf0b897454d01405a2cc9a0831c8481
katsunori-waragai <huyuhiko1128@gmail.com>
2024-11-14 15:30:30 +0900

    worked

--- circular.py
+++ circular.py
@@ -1,10 +1,12 @@
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser("check for opencv-python import")
-    args.add_argument("full_version", help="full version")
+    parser.add_argument("full_version", help="full version")
+    args = parser.parse_args()
     try:
         import cv2
         with open("succeeded.txt", "at") as f:
+            f.write(f"{args.full_version=} : ")
             f.write(f"{cv2.__version__=} \n")
     except ImportError:
         with open("failed.txt", "at") as f:
--- do_all.sh
+++ do_all.sh
@@ -3,5 +3,5 @@ for v in $(cat candidate.txt)
 do
   echo "#" $v;
   echo python3 -m pip install opencv-python==$v;
-  echo python3 circular.py --full_version $v;
+  echo python3 circular.py $v;
 done
